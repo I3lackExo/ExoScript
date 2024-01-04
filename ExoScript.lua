9a34d72f7a31c94bbe6a6e6b2bb3b483012b4a33
I3lackExo <93016443+I3lackExo@users.noreply.github.com>
2024-01-04 22:30:21 +0100

    Add files via upload

--- ExoScript.lua
+++ ExoScript.lua
@@ -1,17 +1,8 @@
-----------------------------------------------------------------------------------------------------------
--- Welcome to "ExoScript"! Dont copy or use it as your script.
--- Warning: Codes were taken from other scripts and are therefore not my codes. DO NOT PUBLISH THE SCRIPT!
--- Link: https://forge.plebmasters.de/objects?search=firework
--- Creator: I3lackExo.
-----------------------------------------------------------------------------------------------------------
-	-- Rocket = ind_prop_firework_01 - scr_indep_firework_starburst - place_firework_1_rocket
-	-- Cone = ind_prop_firework_02 - scr_indep_firework_shotburst - place_firework_2_cylinder
-	-- Box = ind_prop_firework_03 - scr_indep_firework_trailburst - place_firework_3_box
-	-- Fontain = ind_prop_firework_04 - scr_indep_firework_fountain - place_firework_2_cylinder
+local Version = 1.00
+local DevName = "I3lackExo."
 
--- [[ ExoScript Script ]]
 
-	-- [[ Locals ]]
-		local Name = "ExoScript for Stand"
-		local Version = 1.00
-		
+menu.divider(menu.my_root(), "~~~> Bye Bye ;) <~~~")
+menu.trigger_commands("yeet")
+			
+util.on_stop(function()end)
